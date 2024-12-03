fa55983d15c5660b10eb8d10e8e87dade1707600
Pratikg27 <pratikgunjal2127@gmail.com>
2024-12-03 16:18:19 +0530

    origin

--- demo.java
+++ demo.java
@@ -1,5 +1,15 @@
-class demo{
+/*class demo{
     public static void main(String[] args) {
         System.out.println("hello Pratik");
+    }
+}
+*/
+
+class demo{
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 6;
+        int c = a+b;
+        System.out.println(c);
     }
 }
